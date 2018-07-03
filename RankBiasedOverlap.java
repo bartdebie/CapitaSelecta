26b075e2f4956047974e4f3ae7ba551f0de1ff92
bartdebie <36626651+bartdebie@users.noreply.github.com>
2018-07-03 14:28:18 +0200

    finished RBO

--- RankBiasedOverlap.java
+++ RankBiasedOverlap.java
@@ -1,10 +1,13 @@
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -25,26 +28,51 @@ public class RankBiasedOverlap {
         String csvSplitBy = ",";
         
         String currentMetric = "";
-        int metric = -1;
+        int metricNr = -1;
         ArrayList<ArrayList<Model>> models = new ArrayList<ArrayList<Model>>();
         while ((line = br.readLine()) != null) {
             String[] model = line.split(csvSplitBy);
-            if (!(model[0].equals(currentMetric))){
-                currentMetric = model[0];
-                metric++;
+            if (!(model[1].equals(currentMetric))){
+                currentMetric = model[1];
+                metricNr++;
                 models.add(new ArrayList<Model>());
             }
-            models.get(metric).add(new Model(model[0], model[1], Integer.parseInt(model[2])));            
+            models.get(metricNr).add(new Model(model[1], model[0], Integer.parseInt(model[2])));            
         }
 
         // do RBO
+        double[][] RBOvalues = new double[models.size()][models.size()];
+        
         double p = 0.5;
-        RBO(models.get(0), models.get(1), p);
+        for (int i = 0; i < models.size(); i++){
+            ArrayList<Model> metric1 = models.get(i);
+            for (int j = 0; j < models.size(); j++){
+                ArrayList<Model> metric2 = models.get(j);
+                RBOvalues[i][j] = RBO(metric1, metric2, p);
+            }
+        }
         
+        DecimalFormat dm = new DecimalFormat("#.####"); 
+        File file = new File("C:\\Users\\bmcdb\\Dropbox\\_Master algemeen\\17-18 Q4\\Capita selecta\\RBOvalues.txt");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
         
+        writer.write("RBOvalues\t");
+        for (int i = 0; i < models.size(); i++){
+            writer.write(models.get(i).get(0).metric + "\t");
+        }
+        
+        for (int i = 0; i<RBOvalues.length; i++){
+            writer.newLine();
+            writer.write(models.get(i).get(0).metric+"\t");
+            for (int j =0; j<RBOvalues.length; j++){
+                writer.write(dm.format(RBOvalues[i][j])+"\t");
+            }
+        }
+                
+        writer.close();
     }
 
-    public static void RBO(ArrayList<Model> ranking1, ArrayList<Model> ranking2, double p){        
+    public static double RBO(ArrayList<Model> ranking1, ArrayList<Model> ranking2, double p){        
                
         ArrayList<String> temp1 = new ArrayList<>();
         ArrayList<String> temp2 = new ArrayList<>();
@@ -53,11 +81,9 @@ public class RankBiasedOverlap {
         double RBO = 0.0;
         double w_dSum = 0.0;
         
-        if(ranking1.size()!=ranking2.size()){
-            System.out.println("different size rankings!");
-        }
+        int maxDepth = Math.max(ranking1.size(), ranking2.size());
         
-        while (i < ranking2.size()) {// extend to look at smallest set of models
+        while (i < maxDepth) {// extend to look at smallest set of models
             int d = i+1;
             double w_d = (1-p)*Math.pow(p, d-1);
             w_dSum += w_d;
@@ -74,11 +100,13 @@ public class RankBiasedOverlap {
             double agreement = 2.0*intersection(temp1, temp2).size()/(temp1.size()+temp2.size());
             RBO += w_d * agreement;
             i++;
+
         }
         
-        System.out.println("RBO: " + RBO);
-        System.out.println("Sum of weights: " + w_dSum);
-        System.out.println("Scaled RBO: " + RBO / w_dSum);
+//        System.out.println("RBO: " + RBO);
+//        System.out.println("Sum of weights: " + w_dSum);
+//        System.out.println("Scaled RBO: " + RBO / w_dSum);
+        return RBO / w_dSum;
     }
     
     
